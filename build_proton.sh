fff1ccf09bc54364f03f740494d0c9bddb12d42f
Mayeul Cantan <mayeul.cantan@gmail.com>
2018-09-08 00:01:18 +0200

    Rename proton to proton.py

--- build_proton.sh
+++ build_proton.sh
@@ -672,7 +672,7 @@ if [ "$PACKAGE" = true ]; then
 
     cp -a toolmanifest.vdf dist/
     cp -a filelock.py dist/
-    cp -a proton dist/
+    cp -a proton.py dist/
     cp -a user_settings.sample.py dist/
     if [ "$PLATFORM" == "Darwin" ]; then
         cp -a dist.LICENSE.osx dist/LICENSE
