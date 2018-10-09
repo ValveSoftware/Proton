dff92b79a61bb301694de8b57eaf78cd82cd8c9d
Andrew Eikum <aeikum@codeweavers.com>
2018-10-09 10:33:41 -0500

    proton: Track prefix files

--- build_proton.sh
+++ build_proton.sh
@@ -673,6 +673,7 @@ if [ "$PACKAGE" = true ]; then
     cp -a toolmanifest.vdf dist/
     cp -a filelock.py dist/
     cp -a user_settings.sample.py dist/
+    cp -a proton_3.7_tracked_files dist/
     if [ "$PLATFORM" == "Darwin" ]; then
         cp -a dist.LICENSE.osx dist/LICENSE
         sed -e 's/@PYTHON_NAME@/python/' proton.in > dist/proton
