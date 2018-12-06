f01a5d22391c37e559b2dac086c4622c3637f372
Andrew Eikum <aeikum@codeweavers.com>
2018-12-06 14:54:41 -0600

    vagrant-user-setup.sh: No longer need docker branch of steam-runtime

--- vagrant-user-setup.sh
+++ vagrant-user-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #set up steam runtimes. remove these lines if building without the runtime
-git clone https://github.com/ValveSoftware/steam-runtime.git -b wip-docker
+git clone https://github.com/ValveSoftware/steam-runtime.git
 ./steam-runtime/setup_docker.sh --beta amd64 --extra-bootstrap=/home/vagrant/proton/steamrt-bootstrap.sh steam-proton-dev
 ./steam-runtime/setup_docker.sh --beta i386 --extra-bootstrap=/home/vagrant/proton/steamrt-bootstrap.sh steam-proton-dev32
 
