85bbfb60d717c7c901653935e8549b2baa90b5de
Andrew Eikum <aeikum@codeweavers.com>
2018-12-20 09:23:45 -0600

    vagrant-user-setup.sh: Use the beta runtime
    This gives us access to gnutls30. If building in an existing system,
    you'll need to set up new beta runtimes and configure proton to build
    against them. See vagrant-user-setup.sh.

--- vagrant-user-setup.sh
+++ vagrant-user-setup.sh
@@ -2,12 +2,12 @@
 
 #set up steam runtimes. remove these lines if building without the runtime
 git clone https://github.com/ValveSoftware/steam-runtime.git
-./steam-runtime/setup_docker.sh amd64 --extra-bootstrap=/home/vagrant/proton/steamrt-bootstrap.sh steam-proton-dev
-./steam-runtime/setup_docker.sh i386 --extra-bootstrap=/home/vagrant/proton/steamrt-bootstrap.sh steam-proton-dev32
+./steam-runtime/setup_docker.sh --beta amd64 --extra-bootstrap=/home/vagrant/proton/steamrt-bootstrap.sh steam-proton-dev-beta
+./steam-runtime/setup_docker.sh --beta i386 --extra-bootstrap=/home/vagrant/proton/steamrt-bootstrap.sh steam-proton-dev32-beta
 
 #configure proton build
 mkdir build
 cd build
-../proton/configure.sh --steam-runtime64=docker:steam-proton-dev --steam-runtime32=docker:steam-proton-dev32
+../proton/configure.sh --steam-runtime64=docker:steam-proton-dev-beta --steam-runtime32=docker:steam-proton-dev32-beta
 #if building without the runtime, use:
 #../proton/configure.sh --no-steam-runtime
