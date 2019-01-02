ValveSoftware/Proton

commit a8968fe30a102f5b5406e9b97b29c7b789d29455

Perform conversion between more lin/win paths

lsteamclient/steamclient_main.c
@@ -16,6 +16,10 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(steamclient);
 
+char g_tmppath[PATH_MAX];
+
+static char *controller_glyphs[512]; /* at least k_EControllerActionOrigin_Count */
+
 BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, void *reserved)
 {
     TRACE("(%p, %u, %p)\n", instance, reason, reserved);
@@ -30,30 +34,107 @@ BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, void *reserved)
     return TRUE;
 }
 
-uint32 steamclient_unix_path_to_dos_path(uint32 api_result, char *inout, uint32 inout_bytes)
+/* returns the number of bytes written to dst, not including the NUL terminator */
+unsigned int steamclient_unix_path_to_dos_path(bool api_result, const char *src, char *dst, uint32 dst_bytes)
 {
-    WCHAR *converted;
+    WCHAR *dosW;
     uint32 r;
 
-    if(api_result == 0)
+    *dst = 0;
+
+    if(!src || !api_result)
         return 0;
 
-    converted = wine_get_dos_file_name(inout);
-    if(!converted){
-        WARN("Unable to convert unix filename to DOS: %s\n", inout);
-        *inout = 0;
+    dosW = wine_get_dos_file_name(src);
+    if(!dosW){
+        WARN("Unable to convert unix filename to DOS: %s\n", src);
         return 0;
     }
 
-    r = WideCharToMultiByte(CP_UNIXCP, 0, converted, -1, inout, inout_bytes,
+    r = WideCharToMultiByte(CP_UNIXCP, 0, dosW, -1, dst, dst_bytes,
             NULL, NULL);
 
-    HeapFree(GetProcessHeap(), 0, converted);
+    HeapFree(GetProcessHeap(), 0, dosW);
 
-    if(r > 0)
-        return r - 1;
+    return r == 0 ? 0 : r - 1;
+}
 
-    return 0;
+/* returns non-zero on success, zero on failure */
+bool steamclient_dos_path_to_unix_path(const char *src, char *dst)
+{
+    WCHAR srcW[PATH_MAX];
+    char *unix_fn;
+    uint32 r;
+
+    *dst = 0;
+
+    if(!src)
+        return 0;
+
+    r = MultiByteToWideChar(CP_UNIXCP, 0, src, -1, srcW, PATH_MAX);
+    if(r == 0)
+        return 0;
+
+    unix_fn = wine_get_unix_file_name(srcW);
+    if(!unix_fn){
+        WARN("Unable to convert DOS filename to unix: %s\n", src);
+        return 0;
+    }
+
+    strncpy(dst, unix_fn, PATH_MAX);
+
+    HeapFree(GetProcessHeap(), 0, unix_fn);
+
+    return 1;
+}
+
+const char **steamclient_dos_to_unix_stringlist(const char **src)
+{
+    size_t len;
+    const char **s;
+    char **out, **o;
+    WCHAR scratch[PATH_MAX];
+
+    if(!src)
+        return NULL;
+
+    len = sizeof(char*); /* NUL */
+    for(s = src; *s; ++s)
+        len += sizeof(char*);
+
+    out = HeapAlloc(GetProcessHeap(), 0, len);
+
+    for(s = src, o = out; *s; ++s, ++o){
+        MultiByteToWideChar(CP_UNIXCP, 0, *s, -1, scratch, sizeof(scratch)/sizeof(*scratch));
+        *o = wine_get_unix_file_name(scratch);
+    }
+
+    *o = NULL;
+
+    return (const char **)out;
+}
+
+void steamclient_free_stringlist(const char **out)
+{
+    if(out){
+        const char **o;
+        for(o = out; *o; o++)
+            HeapFree(GetProcessHeap(), 0, (char *)*o);
+        HeapFree(GetProcessHeap(), 0, out);
+    }
+}
+
+const char *steamclient_isteamcontroller_getglyph(int origin, const char *lin_path)
+{
+    if(!lin_path)
+        return NULL;
+
+    if(!controller_glyphs[origin])
+        controller_glyphs[origin] = HeapAlloc(GetProcessHeap(), 0, PATH_MAX);
+
+    steamclient_unix_path_to_dos_path(1, lin_path, controller_glyphs[origin], PATH_MAX);
+
+    return controller_glyphs[origin];
 }
 
 #include "win_constructors.h"

lsteamclient/cb_converters.dat
@@ -35,6 +35,7 @@ case 0x00100d56: win_msg->m_cubParam = 24; win_msg->m_pubParam = HeapAlloc(GetPr
 case 0x00100d57: win_msg->m_cubParam = 24; win_msg->m_pubParam = HeapAlloc(GetProcessHeap(), 0, win_msg->m_cubParam); cb_RemoveAppDependencyResult_t_24(lin_msg.m_pubParam, win_msg->m_pubParam); break;
 case 0x00940d58: win_msg->m_cubParam = 152; win_msg->m_pubParam = HeapAlloc(GetProcessHeap(), 0, win_msg->m_cubParam); cb_GetAppDependenciesResult_t_152(lin_msg.m_pubParam, win_msg->m_pubParam); break;
 case 0x000c0d59: win_msg->m_cubParam = 16; win_msg->m_pubParam = HeapAlloc(GetProcessHeap(), 0, win_msg->m_cubParam); cb_DeleteItemResult_t_16(lin_msg.m_pubParam, win_msg->m_pubParam); break;
+case 0x000c11a4: win_msg->m_cubParam = 12; win_msg->m_pubParam = HeapAlloc(GetProcessHeap(), 0, win_msg->m_cubParam); cb_HTML_FileOpenDialog_t_12(lin_msg.m_pubParam, win_msg->m_pubParam); break;
 case 0x00141260: win_msg->m_cubParam = 24; win_msg->m_pubParam = HeapAlloc(GetProcessHeap(), 0, win_msg->m_cubParam); cb_SteamInventoryStartPurchaseResult_t_24(lin_msg.m_pubParam, win_msg->m_pubParam); break;
 case 0x002000d1: win_msg->m_cubParam = 40; win_msg->m_pubParam = HeapAlloc(GetProcessHeap(), 0, win_msg->m_cubParam); cb_GSReputation_t_40(lin_msg.m_pubParam, win_msg->m_pubParam); break;
 case 0x00140835: win_msg->m_cubParam = 24; win_msg->m_pubParam = HeapAlloc(GetProcessHeap(), 0, win_msg->m_cubParam); cb_HTTPRequestCompleted_t_24(lin_msg.m_pubParam, win_msg->m_pubParam); break;

lsteamclient/cb_converters.h
@@ -72,6 +72,8 @@ extern void cb_GetAppDependenciesResult_t_152(void *l, void *w);
 
 extern void cb_DeleteItemResult_t_16(void *l, void *w);
 
+extern void cb_HTML_FileOpenDialog_t_12(void *l, void *w);
+
 extern void cb_SteamInventoryStartPurchaseResult_t_24(void *l, void *w);
 
 extern void cb_GSReputation_t_40(void *l, void *w);

lsteamclient/cb_getapi_sizes.dat
@@ -78,5 +78,7 @@ case 3417: /* DeleteItemResult_t_16 */
     return 12;
 case 4704: /* SteamInventoryStartPurchaseResult_t_24 */
     return 20;
+case 4516: /* HTML_FileOpenDialog_t_12 */
+    return 12;
 case 1023: /* FileDetailsResult_t_40 */
     return 36;

lsteamclient/cb_getapi_table.dat
@@ -247,6 +247,12 @@ case 4704:
     case 24: cb_SteamInventoryStartPurchaseResult_t_24(lin_callback, callback); break;
     }
     break;
+case 4516:
+    switch(callback_len){
+    default:
+    case 12: cb_HTML_FileOpenDialog_t_12(lin_callback, callback); break;
+    }
+    break;
 case 1023:
     switch(callback_len){
     default:

lsteamclient/gen_wrapper.py
@@ -164,6 +164,188 @@ print_sizes = []
 
 class_versions = {}
 
+path_conversions = [
+        {
+            "parent_name": "GetAppInstallDir",
+            "l2w_names": ["pchDirectory"],
+            "l2w_lens": ["cchNameMax"],
+            "w2l_names": [],
+            "w2l_arrays": [],
+            "return_is_size": True
+        },
+        {
+            "parent_name": "GetAppInstallDir",
+            "l2w_names": ["pchFolder"],
+            "l2w_lens": ["cchFolderBufferSize"],
+            "w2l_names": [],
+            "w2l_arrays": [],
+            "return_is_size": True
+        },
+        {
+            "parent_name": "GetFileDetails",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pszFileName"],
+            "w2l_arrays": [False],
+            "return_is_size": True
+        },
+        {
+            "parent_name": "GetGlyphForActionOrigin",
+            "l2w_names": [None], #return value
+            "l2w_lens": [None],
+            "w2l_names": [],
+            "w2l_arrays": [],
+            "return_is_size": False
+        },
+        ### ISteamGameServer::SetModDir - "Just the folder name, not the whole path. I.e. "Spacewar"."
+        {
+            "parent_name": "FileLoadDialogResponse",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchSelectedFiles"],
+            "w2l_arrays": [True],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "HTML_FileOpenDialog_t",
+            "l2w_names": ["pchInitialFile"],
+            "l2w_lens": [None],
+            "w2l_names": [],
+            "w2l_arrays": [],
+            "return_is_size": False
+        },
+        ### XXX: some URLs could be file: protocol
+        {
+            "parent_name": "PublishWorkshopFile",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchFile", "pchPreviewFile"],
+            "w2l_arrays": [False, False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "UpdatePublishedFileFile",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchFile"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "UpdatePublishedFilePreviewFile",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchPreviewFile"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "PublishVideo",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchPreviewFile"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "AddScreenshotToLibrary",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchFilename", "pchThumbnailFilename"],
+            "w2l_arrays": [False, False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "AddVRScreenshotToLibrary",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchFilename", "pchVRFilename"],
+            "w2l_arrays": [False, False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "UGCDownloadToLocation",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchLocation"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        ### is GetQueryUGCAdditionalPreview URL a file:?
+        {
+            "parent_name": "SetItemContent",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pszContentFolder"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "SetItemPreview",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pszPreviewFile"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "AddItemPreviewFile",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pszPreviewFile"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "UpdateItemPreviewFile",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pszPreviewFile"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "GetItemInstallInfo",
+            "l2w_names": ["pchFolder"],
+            "l2w_lens": ["cchFolderSize"],
+            "w2l_names": [],
+            "w2l_arrays": [],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "BInitWorkshopForGameServer",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pszFolder"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "GetUserDataFolder",
+            "l2w_names": ["pchBuffer"],
+            "l2w_lens": ["cubBuffer"],
+            "w2l_names": [],
+            "w2l_arrays": [],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "CheckFileSignature",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["szFileName"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        },
+        {
+            "parent_name": "Init",
+            "l2w_names": [],
+            "l2w_lens": [],
+            "w2l_names": ["pchAbsolutePathToControllerConfigVDF"],
+            "w2l_arrays": [False],
+            "return_is_size": False
+        }
+]
+
 def strip_const(typename):
     return typename.replace("const ", "", 1)
 
@@ -184,6 +366,9 @@ def struct_needs_conversion_nocache(struct):
         if field.type.kind == clang.cindex.TypeKind.RECORD and \
                 struct_needs_conversion(field.type):
             return True
+    path_conv = get_path_converter(struct)
+    if path_conv:
+        return True
     return False
 
 def struct_needs_conversion(struct):
@@ -197,6 +382,21 @@ def handle_destructor(cfile, classname, winclassname, method):
     cfile.write("DEFINE_THISCALL_WRAPPER(%s_destructor, 4)\n" % winclassname)
     cfile.write("void __thiscall %s_destructor(%s *_this)\n{/* never called */}\n\n" % (winclassname, winclassname))
     return "destructor"
+
+def get_path_converter(parent):
+    for conv in path_conversions:
+        if conv["parent_name"] in parent.spelling:
+            if None in conv["l2w_names"]:
+                return conv
+            if type(parent) == clang.cindex.Type:
+                children = list(parent.get_fields())
+            else:
+                children = list(parent.get_children())
+            for child in children:
+                if child.spelling in conv["w2l_names"] or \
+                        child.spelling in conv["l2w_names"]:
+                    return conv
+    return None
 
 def handle_method(cfile, classname, winclassname, cppname, method, cpp, cpp_h, existing_methods):
     used_name = method.spelling
@@ -269,13 +469,24 @@ def handle_method(cfile, classname, winclassname, cppname, method, cpp, cpp_h, e
     cpp.write(")\n{\n")
     cpp_h.write(");\n")
 
-    char_param_is_unix_path = "GetAppInstallDir" in used_name or \
-            "GetItemInstallInfo" in used_name
-    path_param_name = None
-    path_size_param_name = None
+    path_conv = get_path_converter(method)
 
-    if char_param_is_unix_path:
-        cfile.write("    uint32 path_result;\n")
+    if path_conv:
+        for i in range(len(path_conv["w2l_names"])):
+            if path_conv["w2l_arrays"][i]:
+                cfile.write("    const char **lin_%s = steamclient_dos_to_unix_stringlist(%s);\n" % (path_conv["w2l_names"][i], path_conv["w2l_names"][i]))
+                # TODO
+                pass
+            else:
+                cfile.write("    char lin_%s[PATH_MAX];\n" % path_conv["w2l_names"][i])
+                cfile.write("    steamclient_dos_path_to_unix_path(%s, lin_%s);\n" % (path_conv["w2l_names"][i], path_conv["w2l_names"][i]))
+        if None in path_conv["l2w_names"]:
+            cfile.write("    const char *path_result;\n")
+        elif path_conv["return_is_size"]:
+            cfile.write("    uint32 path_result;\n")
+        elif len(path_conv["l2w_names"]) > 0:
+            cfile.write("    %s path_result;\n" % method.result_type.spelling)
+
     for param in need_convert:
         if param.type.kind == clang.cindex.TypeKind.POINTER:
             #handle single pointers, but not double pointers
@@ -291,7 +502,7 @@ def handle_method(cfile, classname, winclassname, cppname, method, cpp, cpp_h, e
 
     if method.result_type.kind == clang.cindex.TypeKind.VOID:
         cfile.write("    ")
-    elif char_param_is_unix_path:
+    elif path_conv and (len(path_conv["l2w_names"]) > 0 or path_conv["return_is_size"]):
         cfile.write("    path_result = ")
     elif returns_record:
         cfile.write("    *_r = ")
@@ -324,13 +535,6 @@ def handle_method(cfile, classname, winclassname, cppname, method, cpp, cpp_h, e
                 cpp.write(", ")
             else:
                 first = False
-            if char_param_is_unix_path:
-                if param.type.spelling == "char *":
-                    path_param_name = param.spelling
-                elif not path_param_name is None and \
-                        (param.type.spelling == "uint32" or
-                                param.type.spelling == "int"):
-                    path_size_param_name = param.spelling
             if param.spelling == "":
                 cfile.write(", _%s" % unnamed)
                 cpp.write("(%s)_%s" % (param.type.spelling, unnamed))
@@ -338,6 +542,9 @@ def handle_method(cfile, classname, winclassname, cppname, method, cpp, cpp_h, e
             elif param.type.kind == clang.cindex.TypeKind.POINTER and \
                     param.type.get_pointee().spelling in wrapped_classes:
                 cfile.write(", create_Linux%s(%s)" % (param.type.get_pointee().spelling, param.spelling))
+                cpp.write("(%s)%s" % (param.type.spelling, param.spelling))
+            elif path_conv and param.spelling in path_conv["w2l_names"]:
+                cfile.write(", %s ? lin_%s : NULL" % (param.spelling, param.spelling))
                 cpp.write("(%s)%s" % (param.type.spelling, param.spelling))
             elif param in need_convert:
                 cfile.write(", %s" % param.spelling)
@@ -354,8 +561,22 @@ def handle_method(cfile, classname, winclassname, cppname, method, cpp, cpp_h, e
     cpp.write(");\n")
     if returns_record:
         cfile.write("    return _r;\n")
-    if char_param_is_unix_path and not path_param_name is None and not path_size_param_name is None:
-        cfile.write("    return steamclient_unix_path_to_dos_path(path_result, %s, %s);\n" % (path_param_name, path_size_param_name))
+    if path_conv and len(path_conv["l2w_names"]) > 0:
+        for i in range(len(path_conv["l2w_names"])):
+            if path_conv["l2w_names"][i]:
+                cfile.write("    ")
+                if path_conv["return_is_size"]:
+                    cfile.write("path_result = ")
+                cfile.write("steamclient_unix_path_to_dos_path(path_result, %s, %s, %s);\n" % (path_conv["l2w_names"][i], path_conv["l2w_names"][i], path_conv["l2w_lens"][i]))
+            else:
+                #string is in return value
+                #ISteamController::GetGlyphForActionOrigin is the only user here for now
+                cfile.write("    path_result = steamclient_isteamcontroller_getglyph(eOrigin, path_result);\n")
+        cfile.write("    return path_result;\n")
+    if path_conv:
+        for i in range(len(path_conv["w2l_names"])):
+            if path_conv["w2l_arrays"][i]:
+                cfile.write("    steamclient_free_stringlist(lin_%s);\n" % path_conv["w2l_names"][i])
     cfile.write("}\n\n")
     for param in need_convert:
         if param.type.kind == clang.cindex.TypeKind.POINTER:
@@ -477,8 +698,6 @@ cb_table = {}
 #because of struct packing differences between win32 and linux, we
 #need to convert these structs from their linux layout to the win32
 #layout.
-#TODO: could we optimize this by detecting if the structs are the
-#same layout at generation-time?
 def handle_struct(sdkver, struct):
     members = struct.get_children()
     cb_num = None
@@ -575,6 +794,8 @@ def handle_struct(sdkver, struct):
     cppfile.write("}  __attribute__ ((ms_struct));\n")
     cppfile.write("#pragma pack( pop )\n")
 
+    path_conv = get_path_converter(struct.type)
+
     def handle_field(m, src, dst):
         if m.kind == clang.cindex.CursorKind.FIELD_DECL:
             if m.type.kind == clang.cindex.TypeKind.CONSTANTARRAY:
@@ -584,6 +805,9 @@ def handle_struct(sdkver, struct):
             elif m.type.kind == clang.cindex.TypeKind.RECORD and \
                     struct_needs_conversion(m.type):
                 cppfile.write("    %s_to_%s_struct_%s_%s(&%s->%s, &%s->%s);\n" % (src, dst, m.type.spelling, sdkver, src, m.displayname, dst, m.displayname))
+            elif path_conv and m.displayname in path_conv["l2w_names"]:
+                cppfile.write("    steamclient_unix_path_to_dos_path(1, %s->%s, g_tmppath, sizeof(g_tmppath));\n" % (src, m.displayname))
+                cppfile.write("    %s->%s = g_tmppath;\n" % (dst, m.displayname))
             else:
                 cppfile.write("    %s->%s = %s->%s;\n" % (dst, m.displayname, src, m.displayname))
 

lsteamclient/steam_defs.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <linux/limits.h>
 typedef uint32_t EHTMLMouseButton, EHTMLKeyModifiers;
 
 #ifndef __cplusplus

lsteamclient/steamclient_private.h
@@ -1,4 +1,9 @@
 /* TODO these should be generated */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct __winISteamClient winISteamClient;
 typedef struct __winISteamUser winISteamUser;
 typedef struct __winISteamGameServer winISteamGameServer;
@@ -29,14 +34,24 @@ typedef struct __winISteamParentalSettings winISteamParentalSettings;
 typedef struct __winX winX;
 
 void *create_win_interface(const char *name, void *linux_side);
-unsigned int steamclient_unix_path_to_dos_path(unsigned int api_result, char *inout, unsigned int inout_bytes);
+unsigned int steamclient_unix_path_to_dos_path(bool api_result, const char *src, char *dst, uint32 dst_bytes);
+bool steamclient_dos_path_to_unix_path(const char *src, char *dst);
+const char **steamclient_dos_to_unix_stringlist(const char **src);
+void steamclient_free_stringlist(const char **out);
+const char *steamclient_isteamcontroller_getglyph(int origin, const char *lin_path);
 void *create_LinuxISteamMatchmakingServerListResponse(void *win);
 void *create_LinuxISteamMatchmakingPingResponse(void *win);
 void *create_LinuxISteamMatchmakingPlayersResponse(void *win);
 void *create_LinuxISteamMatchmakingRulesResponse(void *win);
 
+extern char g_tmppath[PATH_MAX];
+
 typedef uint64 SteamAPICall_t; //for ancient SDKs
 bool do_cb_wrap(HSteamPipe pipe, void *linux_side, bool (*cpp_func)(void *, SteamAPICall_t, void *, int, int, bool *), SteamAPICall_t call, void *callback, int callback_len, int cb_expected, bool *failed);
+
+#ifdef __cplusplus
+}
+#endif
 
 #define TRACE WINE_TRACE
 #define ERR WINE_ERR

commit 77d3c01740fac883c65e660048a51e96bc19ad4d

steamclient: Fix possible crash.
Fix a regression caused by a8968fe.

https://github.com/ValveSoftware/Proton/issues/2131

Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

lsteamclient/steamclient_main.c
@@ -40,6 +40,9 @@ unsigned int steamclient_unix_path_to_dos_path(bool api_result, const char *src,
     WCHAR *dosW;
     uint32 r;
 
+    if(!dst || !dst_bytes)
+        return 0;
+
     if(!src || !api_result){
         *dst = 0;
         return 0;