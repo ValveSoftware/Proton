ac7b2068f0c7c96f954831800ac5589933a0b4d5
Andrew Eikum <aeikum@codeweavers.com>
2019-04-09 08:51:22 -0500

    Add mono logging to default logging

--- user_settings.sample.py
+++ user_settings.sample.py
@@ -1,15 +1,24 @@
-#to enable these settings, name this file "user_settings.py"
+#To enable these settings, name this file "user_settings.py".
+
+#Settings here will take effect for all games run in this Proton version.
 
 user_settings = {
-    #logs are saved to $HOME/steam-$STEAM_APP_ID.log, overwriting any previous log with that name
-    "WINEDEBUG": "+timestamp,+pid,+tid,+seh,+debugstr,+module",
+    #Logs are saved to $HOME/steam-<STEAM_GAME_ID>.log, overwriting any previous log with that name.
 
+    #Wine debug logging
+    "WINEDEBUG": "+timestamp,+pid,+tid,+seh,+debugstr,+module,+mscoree",
+
+    #DXVK debug logging
     "DXVK_LOG_LEVEL": "info",
+
+    #wine-mono debug logging (Wine's .NET replacement)
+    "WINE_MONO_TRACE": "E:System.NotImplementedException",
+    #"MONO_LOG_LEVEL": "info",
 
     #Enable DXVK's HUD
 #    "DXVK_HUD": "devinfo,fps",
 
-    #Use gl-based wined3d for d3d11 and d3d10 instead of vulkan-based dxvk
+    #Use OpenGL-based wined3d for d3d11 and d3d10 instead of Vulkan-based DXVK
 #    "PROTON_USE_WINED3D": "1",
 
     #Disable d3d11 entirely
