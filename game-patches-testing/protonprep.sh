e643dbb9199a6b4368f39942bbf1401dd441e505
GloriousEggroll <gloriouseggroll@gmail.com>
2020-03-22 23:41:22 -0600

    cleanup to match TKG's patch order (shoutout/props to TKG)

--- game-patches-testing/protonprep.sh
+++ game-patches-testing/protonprep.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+
+#TKG patch order:
+
+#549 clock monotonic
+#567 bypass compositor
+#718 vulkan childwindow
+#1044 fsync staging
+#1066 fsync spincount
+#1078 - fs hack
+#1125 rawinput
+#1207 LAA
+#1221 winex11-MWM
+#1229 steam client swap
+#1236 protonify rpc
+#1237 protonify
+#1239 steam bits
+#1332 SDL
+#1333 SDL2
+#1339 gamepad additions
+#1375 vr
+#1386 vk bits
+#1387 fs hack integer scaling
+#1391 winevulkan
+#1396 msvcrt native builtin
+#1411 win10
+#1417 dxvk_config
+
+
     # steam_helper patches
     #git checkout steam_helper
     #cd steam_helper
@@ -46,19 +74,20 @@
     -W user32-rawinput-nolegacy \
     -W user32-rawinput-mouse-experimental \
     -W user32-rawinput-hid \
-    -W dinput-SetActionMap-genre \
-    -W dinput-axis-recalc \
-    -W dinput-joy-mappings \
-    -W dinput-reconnect-joystick \
-    -W dinput-remap-joystick \
     -W winex11-key_translation 
+
+# Enable these for now in favor over proton gamepad additions
+#    -W dinput-SetActionMap-genre \
+#    -W dinput-axis-recalc \
+#    -W dinput-joy-mappings \
+#    -W dinput-reconnect-joystick \
+#    -W dinput-remap-joystick \
 
     #WINE FAUDIO
     echo "applying faudio patches"
     patch -Np1 < ../game-patches-testing/faudio-patches/faudio-ffmpeg.patch
 
-    #WINE GAME PATCHES
-
+    ### GAME PATCH SECTION ###
     echo "mech warrior online"
     patch -Np1 < ../game-patches-testing/game-patches/mwo.patch
 
@@ -91,52 +120,73 @@
     echo "fix steep and AC Odyssey fullscreen"
     patch -Np1 < ../game-patches-testing/wine-patches/0001-Add-some-semi-stubs-in-user32.patch
 
-    #WINE FSYNC
-    echo "applying fsync patches"
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-fsync_staging.patch
-    #patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-fsync-spincounts.patch
-
-    #PROTON
-    echo "applying proton patches"
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-steamclient_swap.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-protonify_staging_rpc.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-protonify_staging.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-win10_default.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-dxvk_config.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-steam-bits.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-LAA_staging.patch
-
-    #disabled, not working
-    #echo "mk11 patch"
-    #patch -Np1 < ../game-patches-testing/game-patches/mk11.patch
-
-    echo "clock monotonic, amd ags, hide prefix update"
+    ### END GAME PATCH SECTION ###
+    
+    echo "clock monotonic, amd ags"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-use_clock_monotonic.patch
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-amd_ags.patch
 
-    echo "fullscreen hack"
+    #PROTON
+    #disabled, not working
+    #echo "mk11 patch"
+    #patch -Np1 < ../game-patches-testing/game-patches/mk11.patch
+    
+    echo "bypass compositor"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-FS_bypass_compositor.patch
+
+    echo "applying winevulkan childwindow"
+    patch -Np1 < ../game-patches-testing/wine-patches/winevulkan-childwindow.patch
+
+    #WINE FSYNC
+    echo "applying fsync patches"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-fsync_staging.patch
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-fsync-spincounts.patch
+    
+    echo "fullscreen hack"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/valve_proton_fullscreen_hack-staging.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-hide_wine_prefix_update_window.patch
 
     echo "raw input"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-rawinput.patch
-
-    echo "applying staging patches that need to be applied after proton rawinput and fullscreen hack"
-
+    
     echo "staging winex11-key_translation"
     patch -Np1 < ../wine-staging/patches/winex11-key_translation/0001-winex11-Match-keyboard-in-Unicode.patch
     patch -Np1 < ../wine-staging/patches/winex11-key_translation/0002-winex11-Fix-more-key-translation.patch
     patch -Np1 < ../wine-staging/patches/winex11-key_translation/0003-winex11.drv-Fix-main-Russian-keyboard-layout.patch
+    
+    #echo "mfplat alpha testing patches"
+    #patch -Np1 < ../game-patches-testing/wine-patches/mfplat_rebase_patchset.patch
+
+    echo "LAA"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-LAA_staging.patch
 
     echo "staging winex11-MWM_Decorations"
     patch -Np1 < ../game-patches-testing/proton-hotfixes/proton-staging_winex11-MWM_Decorations.patch
-
+    
     # staging winex11-_NET_ACTIVE_WINDOW - disabled, currently not working
     #patch -Np1 < ../game-patches-testing/proton-hotfixes/proton-staging_winex11-_NET_ACTIVE_WINDOW.patch
 
     # staging winex11-WM_WINDOWPOSCHANGING - disabled, currently not working
     #patch -Np1 < ../game-patches-testing/proton-hotfixes/proton-staging_winex11-WM_WINDOWPOSCHANGING.patch
+
+    echo "steamclient swap"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-steamclient_swap.patch
+
+    echo "protonify rpc"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-protonify_staging_rpc.patch
+
+    echo "protonify"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-protonify_staging.patch
+    
+    echo "steam bits"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-steam-bits.patch
+
+    echo "SDL Joystick"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-sdl_joy.patch
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-sdl_joy_2.patch
+    
+    #disable for now
+    #echo "proton gamepad additions"
+    #patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-gamepad-additions.patch
 
     echo "Valve VR patches"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-vr.patch
@@ -146,30 +196,25 @@
 
     echo "FS Hack integer scaling"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton_fs_hack_integer_scaling.patch
-
-    echo "SDL Joystick"
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-sdl_joy.patch
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-sdl_joy_2.patch
-
-    echo "proton gamepad additions"
-    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-gamepad-additions.patch
-
+    
+    echo "proton winevulkan"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-winevulkan.patch
+    
     echo "msvcrt overrides"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-msvcrt_nativebuiltin.patch
 
     echo "valve registry entries"
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-apply_LargeAddressAware_fix_for_Bayonetta.patch
     patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-Set_amd_ags_x64_to_built_in_for_Wolfenstein_2.patch
+    
+    echo "set prefix win10"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-win10_default.patch
 
-    #echo "mfplat alpha testing patches"
-    #patch -Np1 < ../game-patches-testing/wine-patches/mfplat_rebase_patchset.patch
+    echo "dxvk_config"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-dxvk_config.patch
 
-    echo "fs hack improvement PRs"
-    patch -Np1 < ../game-patches-testing/wine-patches/winevulkan_fshack_opts.patch
-
-    #WINE VULKAN - must be applied after fshack
-    echo "applying winevulkan patches"
-    patch -Np1 < ../game-patches-testing/wine-patches/winevulkan-childwindow.patch
+    echo "hide wine prefix update"
+    patch -Np1 < ../game-patches-testing/proton-valve-patches/proton-hide_wine_prefix_update_window.patch
 
     echo "applying WoW vkd3d wine patches"
     patch -Np1 < ../game-patches-testing/wine-patches/D3D12SerializeVersionedRootSignature.patch
@@ -178,7 +223,7 @@
     #WINE CUSTOM PATCHES
     #add your own custom patch lines below
 
-
+    ./dlls/winevulkan/make_vulkan
     ./tools/make_requests
     autoreconf -f
 
