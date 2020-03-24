aa3e6d0981a2d251a7b267792d9ab3d0fac2c52f
GloriousEggroll <gloriouseggroll@gmail.com>
2020-03-24 12:58:43 -0600

    merge upstream proton changes

--- vrclient_x64/gen_wrapper.py
+++ vrclient_x64/gen_wrapper.py
@@ -322,7 +322,7 @@ def ivrcompositor_post_present_handoff(cppname, method):
     return "ivrcompositor_post_present_handoff"
 
 def ivrcompositor_wait_get_poses(cppname, method):
-    for version in ["016", "017", "018", "019", "020", "021", "022"]:
+    for version in ["016", "017", "018", "019", "020", "021", "022", "024", "026"]:
         if version in cppname:
             return "ivrcompositor_wait_get_poses"
     return None
