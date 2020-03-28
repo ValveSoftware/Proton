8c105d7aa1ae909f9387792df117239368ccc17d
Rémi Bernon <rbernon@codeweavers.com>
2020-03-28 17:39:20 +0100

    build: Introduce rules-autoconf macro.

---
+++ make/rules-autoconf.mk
@@ -0,0 +1,37 @@
+# parameters:
+#   $(1): lowercase package name
+#   $(2): uppercase package name
+#   $(3): 32/64, build type
+#
+define create-rules-autoconf
+$(call create-rules-common,$(1),$(2),$(3))
+
+ifeq ($(CONTAINER),1)
+$$(OBJ)/.$(1)-configure$(3): $$($(2)_SRC)/configure.ac
+	@echo ":: configuring $(3)bit $(1)..." >&2
+	rm -rf "$$($(2)_OBJ$(3))/config.cache"
+
+	cd "$$($(2)_OBJ$(3))" && env $$($(2)_ENV$(3)) \
+	$$($(2)_SRC)/configure -C \
+	    --prefix="$$($(2)_DST$(3))" \
+	    --libdir="$$($(2)_DST$(3))/lib$(subst 32,,$(3))" \
+	    $$($(2)_ENV$(3)) \
+	    CROSSCFLAGS="$$(COMMON_FLAGS)" \
+	    CROSSCXXFLAGS="$$(COMMON_FLAGS) -std=c++17" \
+	    CROSSLDFLAGS="$$(CROSSLDFLAGS)" \
+	    $$($(2)_CONFIGURE_ARGS) \
+	    $$($(2)_CONFIGURE_ARGS$(3))
+
+	touch $$@
+
+$$(OBJ)/.$(1)-build$(3):
+	@echo ":: building $(3)bit $(1)..." >&2
+	cd "$$($(2)_OBJ$(3))" && env $$($(2)_ENV$(3)) \
+	$$(MAKE)
+	cd "$$($(2)_OBJ$(3))" && env $$($(2)_ENV$(3)) \
+	$$(MAKE) install
+	touch $$@
+endif
+endef
+
+rules-autoconf = $(call create-rules-autoconf,$(1),$(call toupper,$(1)),$(2))
