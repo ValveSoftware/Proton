a31c720af6a3a893c767b007c6db0bb79c89647c
GloriousEggroll <gloriouseggroll@gmail.com>
2020-10-04 22:50:14 -0600

    patches: update mfplat and quartz patches to 5.19

--- patches/wine-hotfixes/backports-for-5.9/mediafoundation/grabpatch.sh
+++ patches/wine-hotfixes/backports-for-5.9/mediafoundation/grabpatch.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-while IFS= read -r LINE; do
-    wget https://github.com/wine-mirror/wine/commit/$LINE.patch
-done < mflist
+if [ $# -eq 0 ]; then
+    echo "Usage: ./grabpatch.txt filename-containing-commit-list"
+    exit 0
+fi
 
 while IFS= read -r LINE; do
-    cat $LINE.patch >> mf_backports_5.17.patch
-done < mflist
+    wget https://github.com/wine-mirror/wine/commit/$LINE.patch
+done < $1
+
+while IFS= read -r LINE; do
+    cat $LINE.patch >> mf_backports_5.19.patch
+done < $1
