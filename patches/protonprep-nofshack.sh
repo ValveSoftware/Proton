91c2447638bb55ca52220b2ebfaefeb074701a73
Thomas Crider <gloriouseggroll@gmail.com>
2021-01-30 21:14:12 -0700

    patches: update protonprep

--- patches/protonprep-nofshack.sh
+++ patches/protonprep-nofshack.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-#TKG patch order:
-
-#549 clock monotonic
-#567 bypass compositor
-#718 vulkan childwindow
-#1044 fsync staging
-#1066 fsync spincount
-#1078 fs hack
-#1125 rawinput
-#1207 LAA
-#1221 winex11-MWM
-#1229 steam client swap
-#1236 protonify rpc
-#1237 protonify
-#1239 steam bits
-#1332 SDL
-#1333 SDL2
-#1339 gamepad additions
-#1375 vr
-#1386 vk bits
-#1387 fs hack integer scaling
-#1391 winevulkan
-#1396 msvcrt native builtin
-#1411 win10
-#1417 dxvk_config
+    cd gst-plugins-base
+    git reset --hard HEAD
+    git clean -xdf
+    echo "add Guy's patch for gstreamer preroll buffer for media converter"
+    patch -Np1 < ../patches/gstreamer/mediaconvert-gstdecodebin2.patch
+    cd ..
 
     cd gst-plugins-ugly
     git reset --hard HEAD
@@ -44,6 +25,11 @@
     cd dxvk
     git reset --hard HEAD
     git clean -xdf
+
+    echo "temporary reverts until 1582 is rebased"
+    git revert --no-commit 2d670ec3db0c1502c84f844e5f9c79f6a5d23c65
+    git revert --no-commit c1073455bfb0ffe19a6f2f487966d44dc05f5f05
+    
     echo "add valve dxvk patches"
     patch -Np1 < ../patches/dxvk/proton-dxvk_avoid_spamming_log_with_requests_for_IWineD3D11Texture2D.patch
     patch -Np1 < ../patches/dxvk/proton-dxvk_add_new_dxvk_config_library.patch
@@ -55,15 +41,11 @@
     patch -Np1 < ../patches/dxvk/dxvk-async.patch
     cd ..
 
-    cd vkd3d-proton
-    git reset --hard HEAD
-    git clean -xdf
-    cd ..
-    
     #WINE STAGING
     cd wine-staging
     git reset --hard HEAD
     git clean -xdf
+    patch -Np1 < ../patches/wine-hotfixes/mfplat_rebase_staging_6.1.patch
     cd ..
 
     #WINE
@@ -72,14 +54,16 @@
     git clean -xdf
     
     # https://bugs.winehq.org/show_bug.cgi?id=49990
-    echo "this reverts bd27af974a21085cd0dc78b37b715bbcc3cfab69 which breaks some game launchers"
+    echo "revert bd27af974a21085cd0dc78b37b715bbcc3cfab69 which breaks some game launchers"
     git revert --no-commit bd27af974a21085cd0dc78b37b715bbcc3cfab69
 
     # revert this in favor of the staging hack, as this breaks dmc and some other games
+    echo "revert 1fceb1213992b79aa7f1a5dc0a72ab3756ee524d part 1 (fixes dmc 5 and some others)"
     git revert --no-commit 1fceb1213992b79aa7f1a5dc0a72ab3756ee524d
     
     # revert this because it breaks controllers on some platforms
     # https://github.com/Frogging-Family/wine-tkg-git/issues/248#issuecomment-760471607
+    echo "revert e4fbae832c868e9fcf5a91c58255fe3f4ea1cb30 which breaks controller detection on some distros"
     git revert --no-commit e4fbae832c868e9fcf5a91c58255fe3f4ea1cb30
 
     # disable these when using proton's gamepad patches
@@ -90,10 +74,9 @@
     # -W dinput-remap-joystick \
 
     # these cause window freezes/hangs with origin
-    # -W winex11-MWM_Decorations \
     # -W winex11-_NET_ACTIVE_WINDOW \
     # -W winex11-WM_WINDOWPOSCHANGING
-        
+
     echo "applying staging patches"
     ../wine-staging/patches/patchinstall.sh DESTDIR="." --all \
     -W dinput-SetActionMap-genre \
@@ -101,13 +84,12 @@
     -W dinput-joy-mappings \
     -W dinput-reconnect-joystick \
     -W dinput-remap-joystick \
-    -W winex11-MWM_Decorations \
     -W winex11-_NET_ACTIVE_WINDOW \
-    -W winex11-WM_WINDOWPOSCHANGING
+    -W winex11-WM_WINDOWPOSCHANGING \
+    -W imm32-com-initialization \
+    -W kernel32-SetProcessDEPPolicy
 
-#    # this is only used when staging disables mfplat if Derek provides an updated rebase
-#    echo "mfplat rebase"
-#    patch -Np1 < ../patches/wine-hotfixes/mfplat_rebase.patch
+    patch -Np1 < ../patches/wine-hotfixes/imm32-com-initialization_no_net_active_window.patch
 
     ### GAME PATCH SECTION ###    
     echo "mech warrior online"
@@ -115,9 +97,6 @@
 
     echo "assetto corsa"
     patch -Np1 < ../patches/game-patches/assettocorsa-hud.patch
-
-#    echo "assetto corsa"
-#    patch -Np1 < ../patches/game-patches/wrc9.patch
 
     echo "origin downloads fix" 
     patch -Np1 < ../patches/game-patches/origin-downloads_fix.patch
@@ -133,19 +112,10 @@
     echo "killer instinct vulkan fix"
     patch -Np1 < ../patches/game-patches/killer-instinct-winevulkan_fix.patch
 
-    echo "SKSE64 updated fix"
-    patch -Np1 < ../patches/game-patches/skse64_fix.patch
-
     # Disabled for now -- non-steam game, needs double check, may be fixed already
     # echo "Paul's Diablo 1 menu fix"
     # patch -Np1 < ../patches/game-patches/diablo_1_menu.patch
-    
-    # warning: currently you WILL get banned, despite this patch fixing timestamps and allowing the game to run.
-    # https://bugs.winehq.org/show_bug.cgi?id=44061
-    # disabled for now to prevent people from getting banned
-    # echo "Destiny 2 runtime timestamp fix"
-    # patch -Np1 < ../patches/game-patches/destiny2_runtime_timestamp_fix.patch
-    
+
     ### END GAME PATCH SECTION ###
     
     ### PROTON PATCH SECTION ###
@@ -153,9 +123,6 @@
     echo "clock monotonic"
     patch -Np1 < ../patches/proton/01-proton-use_clock_monotonic.patch
     
-    echo "bypass compositor"
-    patch -Np1 < ../patches/proton/02-proton-FS_bypass_compositor.patch
-
     #WINE FSYNC
     echo "applying fsync patches"
     patch -Np1 < ../patches/proton/03-proton-fsync_staging.patch
@@ -177,10 +144,13 @@
 
     echo "proton gamepad additions"
     patch -Np1 < ../patches/proton/15-proton-gamepad-additions.patch
+    
+    echo "bypass compositor"
+    patch -Np1 < ../patches/proton/02-proton-FS_bypass_compositor.patch
 
     echo "Valve VR patches"
     patch -Np1 < ../patches/proton/16-proton-vrclient.patch
-    
+
     echo "proton winevulkan"
     patch -Np1 < ../patches/proton/17-proton-winevulkan-nofshack.patch
 
@@ -221,9 +191,6 @@
 #    echo "proton-specific winegstreamer patches"
 #    patch -Np1 < ../patches/proton/34-proton-winegstreamer_updates.patch
 
-    # zf gstreamer pending patches
-    patch -Np1 < ../patches/wine-hotfixes/zf-gstreamer-pending.patch
-    
     echo "proton udev container patches"
     patch -Np1 < ../patches/proton/35-proton-udev_container_patches.patch
     
@@ -231,8 +198,11 @@
     patch -Np1 < ../patches/proton/36-proton-overlay_fixes.patch
 
     echo "proton openxr patches"
-    patch -Np1 < ../patches/proton/36-proton-OpenXR-patches.patch
+    patch -Np1 < ../patches/proton/37-proton-OpenXR-patches.patch
     
+    echo "mouse focus fixes"
+    patch -Np1 < ../patches/proton/38-proton-mouse-focus-fixes.patch
+
     ### END PROTON PATCH SECTION ###
 
     ### WINE PATCH SECTION ###
@@ -240,19 +210,23 @@
     echo "applying winevulkan childwindow"
     patch -Np1 < ../patches/wine-hotfixes/winevulkan-childwindow.patch
 
-    echo "applying WoW vkd3d wine patches"
-    patch -Np1 < ../patches/wine-hotfixes/vkd3d/D3D12SerializeVersionedRootSignature.patch
-    patch -Np1 < ../patches/wine-hotfixes/vkd3d/D3D12CreateVersionedRootSignatureDeserializer.patch
-
-    echo "pending fixes"
+    echo "revert 1fceb1213992b79aa7f1a5dc0a72ab3756ee524d part 2 (fixes dmc 5 and some others)"
     patch -Np1 < ../patches/wine-hotfixes/0033-HACK-Switch-between-all-selection-streams-on-MF_SOUR.patch
+    
+    echo "https://bugs.winehq.org/show_bug.cgi?id=50581 fix"
+    patch -Np1 < ../patches/wine-hotfixes/198992.patch
+    
 
-    #https://bugs.winehq.org/show_bug.cgi?id=50448
-    patch -Np1 < ../patches/wine-hotfixes/ntdll-NtAlertThreadByThreadId_hotfix.patch
-    
-    #https://bugs.winehq.org/show_bug.cgi?id=21014
-    patch -Np1 < ../patches/wine-hotfixes/dsound-pending.patch
-    
+    # more pending experimental work. disabled for now.
+#    echo "Cyberpunk proton shared memory patches"
+#    patch -Np1 < ../patches/proton/proton-cyberpunk-shared-memory.patch
+
+#    echo "Cyberpunk proton thread priority patches"
+#    patch -Np1 < ../patches/proton/proton-cyberpunk-thread-priority.patch
+
+#    echo "Cyberpunk proton futex2 patches"
+#    patch -Np1 < ../patches/proton/proton-cyberpunk-futex2.patch
+
     ### END WINEPATCH SECTION ###
 
     #WINE CUSTOM PATCHES
