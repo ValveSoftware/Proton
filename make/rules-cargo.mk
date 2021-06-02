92ecb97bcfd15345de376a2dc01bea27b0799f3d
Rémi Bernon <rbernon@codeweavers.com>
2021-06-02 15:26:20 +0200

    build: Use $$(OBJ) consistently in rules macros.

--- make/rules-cargo.mk
+++ make/rules-cargo.mk
@@ -7,11 +7,11 @@ define create-rules-cargo
 $(call create-rules-common,$(1),$(2),$(3))
 
 ifeq ($(CONTAINER),1)
-$(OBJ)/.$(1)-configure$(3):
+$$(OBJ)/.$(1)-configure$(3):
 	@echo ":: configuring $(3)bit $(1)..." >&2
 	touch $$@
 
-$(OBJ)/.$(1)-build$(3):
+$$(OBJ)/.$(1)-build$(3):
 	@echo ":: building $(3)bit $(1)..." >&2
 	cd $$($(2)_SRC) && env $$($(2)_ENV$(3)) \
 	cargo build $$(filter -j%,$$(MAKEFLAGS)) --target "$$(ARCH$(3))-unknown-linux-gnu" --target-dir $$($(2)_OBJ$(3)) $$(CARGO_BUILD_ARG)
