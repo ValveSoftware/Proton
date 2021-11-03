2983a4478139ef83c25647d237306e26277de281
Arkadiusz Hiler <ahiler@codeweavers.com>
2021-11-03 19:48:25 +0200

    build: Respect concurrency limit when building with cargo.

--- make/rules-cargo.mk
+++ make/rules-cargo.mk
@@ -14,7 +14,7 @@ $(OBJ)/.$(1)-configure$(3):
 $(OBJ)/.$(1)-build$(3):
 	@echo ":: building $(3)bit $(1)..." >&2
 	cd $$($(2)_SRC) && env $$($(2)_ENV$(3)) \
-	cargo build --target "$$(ARCH$(3))-unknown-linux-gnu" --target-dir $$($(2)_OBJ$(3)) $$(CARGO_BUILD_ARG)
+	cargo build $$(filter -j%,$$(MAKEFLAGS)) --target "$$(ARCH$(3))-unknown-linux-gnu" --target-dir $$($(2)_OBJ$(3)) $$(CARGO_BUILD_ARG)
 	touch $$@
 endif
 endef
