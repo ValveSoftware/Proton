6def823481abc0ab21bf3b6622b89dc26998f5a5
GloriousEggroll <gloriouseggroll@gmail.com>
2023-01-18 11:58:47 -0700

    Remove deprecated broken dxvk-async patch
    Upstream DXVK has implemented the GraphicsPipelineLibrary (GPL) back in August, which takes over dxvk-async's job:
    
    https://www.khronos.org/blog/reducing-draw-time-hitching-with-vk-ext-graphics-pipeline-library
    https://github.com/doitsujin/dxvk/issues/2798
    
    Driver support for Nvidia was added in version 515.49.10:
    https://developer.nvidia.com/vulkan-driver
    
    Driver support for AMD/RADV was added in August 2022 and is an ongoing WIP:
    https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17542
    
    Per the above, it can be exposed for testing on AMD/RADV via:
    RADV_PERFTEST=gpl
    
    dxvk-async now causes problems with the dxvk-cache, which is not something new users may know how to clear:
    https://github.com/Sporif/dxvk-async/issues/55
    
    In light of the above, in addition to the current rebase still not working properly, I am removing the dxvk-async patch from Proton-GE.
    
    RIP dxvk-async 2018-2023

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -13,9 +13,6 @@
     # https://github.com/doitsujin/dxvk/pull/2675
     echo "DXVK: [dxgi] Leave fullscreen mode when window looses focus"
     patch -Np1 < ../patches/dxvk/2675.patch
-
-    echo "DXVK: add dxvk async patch"
-    patch -Np1 < ../patches/dxvk/dxvk-async.patch
     
     echo "DXVK: Fix Secret World: Legends patcher crash"
     patch -Np1 < ../patches/dxvk/secret-world.patch
