3539751620a3c27e91b02538905a4b7f2eae4b4a
Rémi Bernon <rbernon@codeweavers.com>
2023-09-20 08:23:56 +0200

    lsteamclient: Build using makedep rules.
    CW-Bug-Id: #22729

--- lsteamclient/steamclient_manual_common.cpp
+++ lsteamclient/steamclient_manual_common.cpp
@@ -12,10 +12,13 @@ WINE_DEFAULT_DEBUG_CHANNEL(steamclient);
 #include "steam_defs.h"
 #pragma push_macro("__cdecl")
 #undef __cdecl
+#pragma push_macro("strncpy")
+#undef strncpy
 #include "steamworks_sdk_153a/steam_api.h"
 #include "steamworks_sdk_153a/steamnetworkingtypes.h"
 
 #pragma pop_macro("__cdecl")
+#pragma pop_macro("strncpy")
 #include "steamclient_private.h"
 
 extern "C" {
---
+++ make/rules-makedep.mk
@@ -0,0 +1,51 @@
+# parameters:
+#   $(1): lowercase package name
+#   $(2): uppercase package name
+#   $(3): 32/64, build type
+#
+define create-rules-makedep
+$(call create-rules-common,$(1),$(2),$(3))
+
+$$(OBJ)/.$(1)-configure$(3):
+	@echo ":: configuring $(3)bit $(1)..." >&2
+
+	sed -e '/^all:$$$$/,$$$$c all:' \
+	    -e '/^SUBDIRS/,/[^\\]$$$$/c SUBDIRS = $$($(2)_SRC)' \
+	    \
+	    -e '/^srcdir/a objdir = $$(WINE_OBJ$(3))' \
+	    -e '/^prefix/c prefix = $$($(2)_DST$(3))' \
+	    -e '/^libdir/c libdir = $$($(2)_LIBDIR$(3))' \
+	    -e '/^toolsdir/c toolsdir = $$(WINE_OBJ$(3))' \
+	    \
+	    -e '/^CFLAGS/c CFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3))' \
+	    -e '/^CPPFLAGS/c CPPFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CPPFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3))' \
+	    -e '/^CXXFLAGS/c CXXFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CXXFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3)) -std=c++17' \
+	    -e '/^LDFLAGS/c LDFLAGS = $$($(2)_LIBFLAGS$(3)) $$($(2)_LDFLAGS$(3)) $$($(2)_LDFLAGS) $$($(4)LDFLAGS)' \
+	    \
+	    -e '/^x86_64_CFLAGS/c x86_64_CFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3))' \
+	    -e '/^x86_64_CPPFLAGS/c x86_64_CPPFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CPPFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3))' \
+	    -e '/^x86_64_CXXFLAGS/c x86_64_CXXFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CXXFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3)) -std=c++17' \
+	    -e '/^x86_64_LDFLAGS/c x86_64_LDFLAGS = $$($(2)_LIBFLAGS$(3)) $$($(2)_LDFLAGS$(3)) $$($(2)_LDFLAGS) $$(CROSSLDFLAGS)' \
+	    \
+	    -e '/^i386_CFLAGS/c i386_CFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3))' \
+	    -e '/^i386_CPPFLAGS/c i386_CPPFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CPPFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3))' \
+	    -e '/^i386_CXXFLAGS/c i386_CXXFLAGS = $$($(2)_INCFLAGS$(3)) $$($(2)_CXXFLAGS) $$(COMMON_FLAGS) $$(COMMON_FLAGS$(3)) -std=c++17' \
+	    -e '/^i386_LDFLAGS/c i386_LDFLAGS = $$($(2)_LIBFLAGS$(3)) $$($(2)_LDFLAGS$(3)) $$($(2)_LDFLAGS) $$(CROSSLDFLAGS)' \
+	    \
+	    $$(WINE_OBJ$(3))/Makefile > $$($(2)_OBJ$(3))/Makefile
+
+	cd "$$($(2)_OBJ$(3))" && env $$($(2)_ENV$(3)) \
+	$$(WINE_OBJ$(3))/tools/makedep
+
+	touch $$@
+
+$$(OBJ)/.$(1)-build$(3):
+	@echo ":: building $(3)bit $(1)..." >&2
+	+cd "$$($(2)_OBJ$(3))" && env $$($(2)_ENV$(3)) \
+	$$(BEAR) $$(MAKE)
+	cd "$$($(2)_OBJ$(3))" && env $$($(2)_ENV$(3)) \
+	$$(MAKE) install
+	touch $$@
+endef
+
+rules-makedep = $(call create-rules-makedep,$(1),$(call toupper,$(1)),$(2))
