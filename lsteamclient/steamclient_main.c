b04135b4801a22bccd1b0a96f46e97f4d9320eb0
Rémi Bernon <rbernon@codeweavers.com>
2023-10-18 19:29:50 +0200

    lsteamclient: Add some missing exports for SDK 1.58.

--- lsteamclient/steamclient_main.c
+++ lsteamclient/steamclient_main.c
@@ -828,6 +828,8 @@ static bool (*steamclient_BGetCallback)(HSteamPipe a, CallbackMsg_t *b, int32 *c
 static bool (*steamclient_GetAPICallResult)(HSteamPipe, SteamAPICall_t, void *, int, int, bool *);
 static bool (*steamclient_FreeLastCallback)(HSteamPipe);
 static void (*steamclient_ReleaseThreadLocalMemory)(int);
+static bool (*steamclient_IsKnownInterface)( const char *pchVersion );
+static void (*steamclient_NotifyMissingInterface)( HSteamPipe hSteamPipe, const char *pchVersion );
 
 static int load_steamclient(void)
 {
@@ -890,6 +892,18 @@ static int load_steamclient(void)
     steamclient_ReleaseThreadLocalMemory = dlsym(steamclient_lib, "Steam_ReleaseThreadLocalMemory");
     if(!steamclient_ReleaseThreadLocalMemory){
         ERR("unable to load ReleaseThreadLocalMemory method\n");
+        return 0;
+    }
+
+    steamclient_IsKnownInterface = dlsym(steamclient_lib, "Steam_IsKnownInterface");
+    if(!steamclient_IsKnownInterface){
+        ERR("unable to load IsKnownInterface method\n");
+        return 0;
+    }
+
+    steamclient_NotifyMissingInterface = dlsym(steamclient_lib, "Steam_NotifyMissingInterface");
+    if(!steamclient_NotifyMissingInterface){
+        ERR("unable to load NotifyMissingInterface method\n");
         return 0;
     }
 
@@ -1111,4 +1125,18 @@ HSteamPipe after_steam_pipe_create(HSteamPipe pipe)
     TRACE("Created callback thread 0x%04x.\n", callback_thread_id);
 
     return pipe;
+}
+
+bool Steam_IsKnownInterface( const char *pchVersion )
+{
+    TRACE("%s\n", pchVersion);
+    load_steamclient();
+    return steamclient_IsKnownInterface( pchVersion );
+}
+
+void Steam_NotifyMissingInterface( HSteamPipe hSteamPipe, const char *pchVersion )
+{
+    TRACE("%u %s\n", hSteamPipe, pchVersion);
+    load_steamclient();
+    steamclient_NotifyMissingInterface( hSteamPipe, pchVersion );
 }
