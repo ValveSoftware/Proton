faf681cd9cb30fe47572f8298f259e1bd630c5cd
Rémi Bernon <rbernon@codeweavers.com>
2024-01-09 18:33:53 +0100

    lsteamclient: Execute any pending callback before ReleaseRequest.
    CW-Bug-Id: #22729

--- lsteamclient/steamclient_private.h
+++ lsteamclient/steamclient_private.h
@@ -40,6 +40,8 @@ typedef struct w_steam_iface *(*iface_constructor)( void * );
 extern iface_constructor find_iface_constructor( const char *iface_version );
 extern struct w_steam_iface *create_winISteamNetworkingFakeUDPPort_SteamNetworkingFakeUDPPort001( void * );
 
+extern void execute_pending_callbacks(void);
+
 struct w_steam_iface *create_win_interface(const char *name, void *linux_side);
 void *alloc_mem_for_iface(size_t size, const char *iface_version);
 void *alloc_vtable(void *vtable, unsigned int method_count, const char *iface_version);
